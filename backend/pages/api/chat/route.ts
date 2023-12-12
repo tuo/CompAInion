31039056683c8e09c44b072fb5ec963d07d7f5b2
Huang Tuo <clarkhtse@gmail.com>
2023-12-12 02:12:15 -0600

    Update route.ts

--- backend/pages/api/chat/route.ts
+++ backend/pages/api/chat/route.ts
@@ -13,12 +13,31 @@ export const config = {
   runtime: "edge",
 };
 
-export default async function POST(req: Request) {
+export default async function handleRequest(req: Request) {
+  switch (req.method) {
+    case 'POST':
+      return POST(req);
+    case 'OPTIONS':
+      return handleOptions(req);
+    default:
+      return new Response(null, { status: 405 });
+  }
+}
+
+async function handleOptions(req: Request) {
+  const headers = new Headers();
+  headers.append('Access-Control-Allow-Origin', '*');
+  headers.append('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  headers.append('Access-Control-Allow-Headers', 'Content-Type');
+  return new Response(null, { headers });
+}
+
+
+async function POST(req: Request) {
   const headers = req.headers;
   const reqJson = await req.json();
   console.log("===reqJson", reqJson);
   const messages = reqJson?.messages;
-
 
   if (headers.get("Authorization") !== password) {
     console.log("Authorized header is not matching the password.");
@@ -27,7 +46,7 @@ export default async function POST(req: Request) {
       body: { error: "Incorrect authorization headers was used." },
     };
   }
-
+  console.log("===messages", messages);
   // Ask OpenAI for a streaming completion given the prompt
   const response = await chatGPTApi.createChatCompletion({
     model: "gpt-3.5-turbo",
